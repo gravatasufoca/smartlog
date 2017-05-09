a14ab39d2815e18d2f58720309c0a2370ceb622d
Bruno Canto <bruno.canto@ctis.com.br>
2017-05-09 14:54:48 -0300

    observer

--- api/v1/classes/service/agendadorService.php
+++ api/v1/classes/service/agendadorService.php
@@ -12,15 +12,16 @@ class AgendadorService
         $this->db = new DbHandler();
     }
 
-    public function enviarRequest(){
-
+    public function enviarRequest()
+    {
+        require_once "classes/helper/FcmHelper.php";
         $resp = $this->db->getList("SELECT ds_chave FROM tb_aparelho WHERE fl_ativo=1");
-        $chaves=array();
-        foreach($resp as $chave){
+        $chaves = array();
+        foreach ($resp as $chave) {
             array_push($chaves, array_pop($resp)["ds_chave"]);
-            if( count($chaves) ==999 or count($resp)==0){
+            if (count($chaves) == 999 or count($resp) == 0) {
                 FcmHelper::sendMessage(array("tipoAcao" => FcmHelper::$OBSERVER), $chaves);
-                $chaves=array();
+                $chaves = array();
             }
         }
     }
